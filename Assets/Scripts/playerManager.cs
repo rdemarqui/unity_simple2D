2564159910b868308bc2439f1b4c29ce864b7ff5
rdemarqui <rildo17@gmail.com>
2023-05-04 22:21:15 -0300

    camera moviment added

--- Assets/Scripts/playerManager.cs
+++ Assets/Scripts/playerManager.cs
@@ -6,12 +6,15 @@ public class playerManager : MonoBehaviour
 {
     private Rigidbody2D player;
     private float movePlayer;
-    private bool jump;
-    public float speed, jumpForce;
+    public float speed, jumpForce, alturaCamera;
+    private bool jump, isgrounded;
+    private GameObject cameraPos;
+
     // Start is called before the first frame update
     void Start()
     {
         player = GetComponent<Rigidbody2D>();
+        cameraPos = GameObject.Find("Main Camera");
     }
 
     // Update is called once per frame
@@ -19,11 +22,21 @@ public class playerManager : MonoBehaviour
     {
         movePlayer = Input.GetAxis("Horizontal");
         jump = Input.GetButtonDown("Jump");
-
+        cameraPos.transform.position = new Vector3(cameraPos.transform.position.x,
+            player.transform.position.y + alturaCamera, cameraPos.transform.position.z);
         player.velocity = new Vector2(movePlayer * speed, player.velocity.y);
-        if (jump == true)
+        
+        if (jump == true && isgrounded == true)
         {
             player.AddForce(new Vector2(0, jumpForce));
+            isgrounded = false;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.layer == 8)
+        {
+            isgrounded = true;
         }
     }
 }
