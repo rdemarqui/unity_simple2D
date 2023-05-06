78bcb6bb84af1c6e5906f13270c7eea77719f3ec
rdemarqui <rildo17@gmail.com>
2023-05-05 21:53:23 -0300

    panel added

--- Assets/Scripts/playerManager.cs
+++ Assets/Scripts/playerManager.cs
@@ -6,20 +6,24 @@ public class playerManager : MonoBehaviour
 {
     private Rigidbody2D player;
     private float movePlayer;
-    public float speed, jumpForce, alturaCamera;
-    private bool jump, isgrounded;
-    private GameObject cameraPos;
+    public float speed, jumpForce, alturaCamera, speedWin;
+    private bool jump, isgrounded, restartPlayer, win;
+    private GameObject cameraPos, inicialPos;
+    public GameObject panelWin;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GetComponent<Rigidbody2D>();
         cameraPos = GameObject.Find("Main Camera");
+        inicialPos = GameObject.Find("inicialPos");
+        win = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        print(win);
         movePlayer = Input.GetAxis("Horizontal");
         jump = Input.GetButtonDown("Jump");
         cameraPos.transform.position = new Vector3(cameraPos.transform.position.x,
@@ -31,12 +35,41 @@ public class playerManager : MonoBehaviour
             player.AddForce(new Vector2(0, jumpForce));
             isgrounded = false;
         }
+        Restart();
+        WinGame();
     }
     private void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.layer == 8)
         {
             isgrounded = true;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("armadilhas") == true)
+        {
+            restartPlayer = true;
+        }
+        if (col.CompareTag("win") == true)
+        {
+            win = true;
+        }
+    }
+    private void Restart()
+    {
+        if (restartPlayer == true)
+        {
+            player.transform.position = new Vector2(inicialPos.transform.position.x, inicialPos.transform.position.y);
+            restartPlayer = false;
+        }
+    }
+    private void WinGame()
+    {
+        if (win == true)
+        {
+            player.velocity = new Vector2(0, player.velocity.y);
+            panelWin.transform.position = Vector2.MoveTowards(panelWin.transform.position, cameraPos.transform.position, speedWin * Time.deltaTime);
         }
     }
 }
